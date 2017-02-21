9735ba4e996fa3923cb554a6d99c3d3fc9a5c9b2
Keith Hoffmann <keith@theuprisingcreative.com>
2017-02-21 13:14:25 -0600

    insert ga string

--- src/document.js
+++ src/document.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { renderToStaticMarkup, renderToString } from 'react-dom/server'
 
 import banner from 'raw-loader!./banner.txt'
+import analytics from 'raw-loader!./ga.js'
 import style from '../style/index.css'
 
 const document = El => {
@@ -9,13 +10,19 @@ const document = El => {
   const html = renderToStaticMarkup(
     <html>
       <head>
+        <meta charset='utf-8' />
+        <meta http-equiv='x-ua-compatible' content='ie=edge' />
         <title>Stagnate</title>
-        <style dangerouslySetInnerHTML={{__html: style}} />
+        <meta name='description' content='' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='apple-touch-icon' href='apple-touch-icon.png' />
+        <style dangerouslySetInnerHTML={{__html: style}} />
       </head>
       <body>
         <div id='root' dangerouslySetInnerHTML={{__html: elHtml}} />
         <script src='/stagnate.js' />
+        <script dangerouslySetInnerHTML={{__html: analytics}} />
+        <script src='https://www.google-analytics.com/analytics.js' async defer />
       </body>
     </html>
   )
